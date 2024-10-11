8e629e57930e12151e04327f52a6dd7de0946cfa
phimmika <pimmika09@gmail.com>
2024-10-11 12:39:23 +0700

    edit bt v3

--- src/MyCard.jsx
+++ src/MyCard.jsx
@@ -27,7 +27,7 @@ export default function MyCard(props) {
   </CardContent>
   <CardActions>
     <Button size="small">Learn More</Button>
-    <Button size="small">Learn More</Button>
+    <Button size="small">fvbfc</Button>
   </CardActions>
 </Card>
 
